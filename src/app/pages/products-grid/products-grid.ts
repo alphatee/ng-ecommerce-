7fd97d2e22f3a339a09e5b7cd61ab1e0c86c3141
alphatee <alphatee.herrera@gmail.com>
2025-12-17 14:41:06 -0800

    rendering the products grid

--- src/app/pages/products-grid/products-grid.ts
+++ src/app/pages/products-grid/products-grid.ts
@@ -6,9 +6,19 @@ import { Product } from '../../layout/models/product';
   imports: [],
   template: `
     <div class="bg-gray-100 p-6">
-      <h1 class="text-2xl font-bold text-gray-900">
-        Category
+      <h1 class="text-2xl font-bold text-gray-900 mb-6">
+        {{ category() }} <!-- text interpolation -->
       </h1>
+
+      <div class="responsive-grid">
+      @for (product of filteredProducts(); track product.id) {
+        <div class="bg-white cursor-pointer rounded-xl shadow-lg overflow-hidden flex flex-col h-full">
+          <img [src]="product.imageUrl" class="w-full h-[300px] object-cover rounded-t-xl" />
+        </div>
+      }
+      </div>
+
+      <!-- Place product info below: 15:40 -->
     </div>
   `,
   styles: ``,
@@ -28,6 +38,28 @@ export default class ProductsGrid {
       reviewCount: 6,
       inStock: true,
       category: 'electronics'
+    },
+    {
+      id: '2',
+      name: "Ultra HD 4K Smart TV",
+      description: "Experience breathtaking clarity with this 55-inch LED display featuring integrated smart streaming apps and HDR support",
+      price: 649.99,
+      imageUrl: "https://images.unsplash.com/photo-1593784991095-a205069470b6?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      rating: 4.5,
+      reviewCount: 12,
+      inStock: true,
+      category: "electronics"
+    },
+    {
+      id: '3',
+      name: "Professional Mirrorless Camera",
+      description: "Capture high-resolution photos and 4K video with lightning-fast autofocus and a compact weather-sealed body",
+      price: 1299.00,
+      imageUrl: "https://images.unsplash.com/photo-1538531048068-aef582996426?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      rating: 4.9,
+      reviewCount: 8,
+      inStock: true,
+      category: "electronics"
     }
   ]);
 
